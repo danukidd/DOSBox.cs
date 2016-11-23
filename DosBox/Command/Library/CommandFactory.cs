2e8da367b57f9b8aa8c4d10fcbcb7c3a0d5511e5
Kyos-PC\Kyo's <herman_1c@yahoo.co.id>
2016-11-23 16:37:22 +0700

    Add Exit

--- DosBox/Command/Library/CommandFactory.cs
+++ DosBox/Command/Library/CommandFactory.cs
@@ -28,7 +28,8 @@ namespace DosBox.Command.Library
                                new CmdMkDir("md", drive),
                                new CmdMkFile("mf", drive),
                                new CmdMkFile("mkfile", drive),
-                               new CmdHelp("help", drive)
+                               new CmdHelp("help", drive),
+                               new CmdExit("exit", drive)
 
                                // Add your commands here
 
